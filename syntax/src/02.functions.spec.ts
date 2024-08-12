55cf4a981a329ec812115cbf8c23bb45c229238d
= <mpk44@pitt.edu>
2024-08-12 15:56:19 -0400

    basic types

--- syntax/src/02.functions.spec.ts
+++ syntax/src/02.functions.spec.ts
@@ -1,6 +1,6 @@
 import { describe, expect, test } from "vitest";
 import { add, doubleAndSumIt } from "./stuff/02.stuff";
-import { sumThem, sumThemWithUnions } from "./stuff/02.stuff2";
+import { calculateTaxForContractor, calculateTaxForFullTime, calculateWeeklyPay, sumThem, sumThemWithUnions, TagMaker, tagMaker } from "./stuff/02.stuff2";
 
 describe('functions and arguments', () => {
     test('params need typing', () => {
@@ -21,5 +21,98 @@ describe('functions and arguments', () => {
 
         sumThemWithUnions(1, 2);
         sumThemWithUnions('dog', 'cat');
+    });
+});
+
+describe('object literals & functions', () => {
+    test('plain object literals', () => {
+        type Actor = {
+            firstName: string;
+            lastName: string;
+            age: number;
+            jedi: boolean;
+            getInfo: () => string;
+        };
+        const luke = {
+            firstName: 'Luke',
+            lastName: 'Skywalker',
+            age: 22,
+            jed: true,
+            getInfo: function() {
+                return `This is ${this.firstName} ${this.lastName}`
+            }
+        };
+
+        const han: Actor = {
+            firstName: 'Han',
+            lastName: 'Solo',
+            age: 33,
+            jedi: false,
+            getInfo: function() {
+                return `This is ${this.firstName} ${this.lastName}`
+            }
+        };
+
+        expect(luke.firstName).toBe('Luke');
+        expect(luke.getInfo()).toBe('This is Luke Skywalker');
+
+        // structural typing for Luke, no explicit type annotation
+        doSomething(luke);
+        doSomething(han);
+
+        function doSomething(who: { lastName: string, getInfo: () => string }) {
+            console.log(who.lastName);
+            console.log(who.getInfo());
+        }
+    });
+
+    test('higher ordered functions', () => {
+        // functions that take 1 or more functions as an argument and/or return a function
+        const numbers = [1, 2, 3, 4, 5];
+
+        const doubler = (n: number) => n * 2;
+        const doubled = numbers.map(doubler);
+
+        expect(doubled).toEqual([2, 4, 6, 8, 10]);
+        expect(numbers).toEqual([1, 2, 3, 4, 5]);
+
+        const payInfo = calculateWeeklyPay(1000, calculateTaxForFullTime);
+
+        expect(payInfo.weeklyPay).toBe(19);
+        expect(payInfo.taxableAmount).toBe(4);
+
+        const payInfo2 = calculateWeeklyPay(1000, calculateTaxForContractor);
+
+        expect(payInfo2.weeklyPay).toBe(19);
+        expect(payInfo2.taxableAmount).toBe(0);
+    });
+
+    test('returning a function', () => {
+        const h1Maker = tagMaker('h1');
+        const h2Maker = tagMaker('h2');
+
+        expect(h1Maker('hello')).toBe('<h1>hello</h1>');
+        expect(h2Maker('goodbye')).toBe('<h2>goodbye</h2>');
+        // can also do the above with just 2 args (content and tag)
+
+        const pMaker = new TagMaker('p');
+
+        expect(pMaker.make('weirdo')).toEqual('<p>weirdo</p>');
+    });
+
+    test('arrays and tuple types', () => {
+        const friends = ['Sean', 'Billy', 'Ed', 'Mo', 1138] as const;
+
+        // let myAssociates: Array<string | number>;
+        // let myAssociates: (string | number)[];
+
+        expect(friends[0].length).toBe(4);
+        expect(friends[4] * 2).toBe(1138 * 2);
+        expect(typeof friends[0]).toBe('string');
+        expect(typeof friends[4]).toBe('number');
+
+        type Entry = [number, string, string[]];
+
+        const entry: Entry = [19, 'dog', ['stuff']];
     });
 });
--- syntax/src/stuff/02.stuff2.ts
+++ syntax/src/stuff/02.stuff2.ts
@@ -3,28 +3,59 @@ export function sumThem(a: string, b: string): number;
 export function sumThem(a: number[], b: number[]): number;
 
 export function sumThem(a: unknown, b: unknown) {
-    if (typeof a === 'number' && typeof b === 'number') {
-        return a + b;
-    }
-    if (typeof a === 'string' && typeof b === 'string') {
-        return a + b;
-    }
-    if (Array.isArray(a) && Array.isArray(b)) {
-        return [...a, ...b].reduce((l, r) => l + r);
-    }
-    return -42;
+  if (typeof a === "number" && typeof b === "number") {
+    return a + b;
+  }
+  if (typeof a === "string" && typeof b === "string") {
+    return a + b;
+  }
+  if (Array.isArray(a) && Array.isArray(b)) {
+    return [...a, ...b].reduce((l, r) => l + r);
+  }
+  return -42;
 }
 
 type SummableItems = number | string | number[];
-export function sumThemWithUnions (a: SummableItems, b: SummableItems) {
-    if (typeof a === 'number' && typeof b === 'number') {
-        return a + b;
+export function sumThemWithUnions(a: SummableItems, b: SummableItems) {
+  if (typeof a === "number" && typeof b === "number") {
+    return a + b;
+  }
+  if (typeof a === "string" && typeof b === "string") {
+    return a + b;
+  }
+  if (Array.isArray(a) && Array.isArray(b)) {
+    return [...a, ...b].reduce((l, r) => l + r);
+  }
+  return -42;
+}
+
+export const calculateTaxForFullTime = (n: number) => n * 0.2;
+export const calculateTaxForContractor = (n: number) => n * 0;
+type TaxablePayCalculation = (n: number) => number;
+export function calculateWeeklyPay(
+  annualPay: number,
+  taxableFn: TaxablePayCalculation
+) {
+  const pay = annualPay / 52;
+  return {
+    weeklyPay: Math.round(pay),
+    taxableAmount: Math.round(taxableFn(pay)),
+  };
+}
+
+export function tagMaker(tag: string) {
+    return (content: string) => `<${tag}>${content}</${tag}>`;
+}
+
+// example of how classes and higher order functions like tagMaker can serve similar purposes
+export class TagMaker {
+    private _tag: string;
+
+    constructor(tag: string) {
+        this._tag = tag;
     }
-    if (typeof a === 'string' && typeof b === 'string') {
-        return a + b;
+
+    make(content: string) {
+        return `<${this._tag}>${content}</${this._tag}>`;
     }
-    if (Array.isArray(a) && Array.isArray(b)) {
-        return [...a, ...b].reduce((l, r) => l + r);
-    }
-    return -42;
 }
